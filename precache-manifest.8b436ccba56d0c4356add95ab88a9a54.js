1d28b38917c057f3ee1c6e52eec1151010748fab
dominikfilip <dominik.filip10@gmail.com>
2019-03-05 09:42:17 +0100

    Updates

---
+++ precache-manifest.8b436ccba56d0c4356add95ab88a9a54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "145b928c1a2591fe89a4ca4f39cabeee",
+    "url": "/static/media/7ninjas.145b928c.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "253f8f16daf50346ccb2",
+    "url": "/static/js/main.253f8f16.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "253f8f16daf50346ccb2",
+    "url": "/static/css/main.02e11393.chunk.css"
+  },
+  {
+    "revision": "b4bb86a75c2226b6bec2d36ee0d11545",
+    "url": "/index.html"
+  }
+];

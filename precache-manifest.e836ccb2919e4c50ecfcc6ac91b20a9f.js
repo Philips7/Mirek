1257a406c0b6d575f2742be0bb93143f47b400f8
dominikfilip <dominik.filip10@gmail.com>
2019-03-05 11:42:55 +0100

    Updates

---
+++ precache-manifest.e836ccb2919e4c50ecfcc6ac91b20a9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "145b928c1a2591fe89a4ca4f39cabeee",
+    "url": "/Mirek/static/media/7ninjas.145b928c.png"
+  },
+  {
+    "revision": "8decd4beb62e2a748860",
+    "url": "/Mirek/static/js/runtime~main.8decd4be.js"
+  },
+  {
+    "revision": "ca98d873068602934e42",
+    "url": "/Mirek/static/js/main.ca98d873.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/Mirek/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "ca98d873068602934e42",
+    "url": "/Mirek/static/css/main.f6eb7652.chunk.css"
+  },
+  {
+    "revision": "f21ae45ee2349c36b4c5d145ed984c8a",
+    "url": "/Mirek/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b436ccba56d0c4356add95ab88a9a54.js"
+  "/Mirek/precache-manifest.e836ccb2919e4c50ecfcc6ac91b20a9f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Mirek/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
